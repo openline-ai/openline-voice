ed943cb5b472ba7fe95a231a4aa8f098e9bbbd37
tsearle <torrey@openline.ai>
2022-11-18 08:56:10 +0100

    12 fix install scripts (#13)

--- deployment/k8s/local-minikube/2-start-tunnels.sh
+++ deployment/k8s/local-minikube/2-start-tunnels.sh
@@ -5,6 +5,7 @@ minikube tunnel --bind-address 127.0.0.1 &
 if [ $(uname -m) == "x86_64" ];
 then
   kubectl port-forward --namespace $NAMESPACE_NAME svc/kamailio-service 8080:8080 &
+  kubectl port-forward --namespace $NAMESPACE_NAME svc/voice-plugin-service 11010:11010 &
 fi
 
 wait
