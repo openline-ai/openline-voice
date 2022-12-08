8188647705b7d790002be13f9b27454261e6f147
tsearle <torrey@openline.ai>
2022-12-08 07:30:19 +0100

    call recording attempt

--- packages/server/asterisk/call_recorder/call_recorder.py
+++ packages/server/asterisk/call_recorder/call_recorder.py
@@ -4,8 +4,20 @@ import grpc
 from proto import messagestore_pb2
 from proto import messagestore_pb2_grpc
 import logging
+import wave
+import sys
 
 def run():
+    n = len(sys.argv)
+    if n != 4:
+        print("Incorrect number of arguments passed got %d expected 4" % n)
+        exit(-1)
+
+    with wave.open("/var/spool/asterisk/monitoring/" + sys.argv[1]) as mywav:
+        duration_seconds = mywav.getnframes() / mywav.getframerate()
+        print(f"Length of the WAV file: {duration_seconds:.1f} s")
+
+    print("Call From %s to %s" % (sys.argv[2], sys.argv[3]))
     with grpc.insecure_channel('localhost:9009') as channel:
         stub = messagestore_pb2_grpc.MessageStoreServiceStub(channel)
         response = stub.saveMessage(messagestore_pb2.Message(message='Call of duration 5 minutes',
