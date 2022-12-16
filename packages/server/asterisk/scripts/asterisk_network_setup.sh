4a7222fbf7f62e1439973ff76ae1992a4560d600
tsearle <torrey@openline.ai>
2022-12-16 09:24:14 +0100

    40 detect public ip in docker asterisk (#41)
    * set public ip
    
    * need to keep track of both ip
    
    * fix typo

--- packages/server/asterisk/scripts/asterisk_network_setup.sh
+++ packages/server/asterisk/scripts/asterisk_network_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-sed "s/!PUBLIC_IP!/$PUBLIC_IP/g" /etc/asterisk/pjsip.conf.template| sed "s/!LOCAL_IP!/$LOCAL_IP/g" > /etc/asterisk/pjsip.conf
+if [ -z "$PUBLIC_IP_EXTERNAL" ]; then
+PUBLIC_IP_EXTERNAL=$PUBLIC_IP
+fi
+
+sed "s/!PUBLIC_IP!/$PUBLIC_IP_EXTERNAL/g" /etc/asterisk/pjsip.conf.template| sed "s/!LOCAL_IP!/$LOCAL_IP/g" > /etc/asterisk/pjsip.conf
 
 sed "s/!PUBLIC_IP!/$PUBLIC_IP/g" /etc/asterisk/rtp.conf.template| sed "s/!LOCAL_IP!/$LOCAL_IP/g" > /etc/asterisk/rtp.conf
