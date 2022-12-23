b21f2499c0f1a45c5b75383031f20afe9733cee1
Gabriel Gontariu <gabi@openline.ai>
2022-12-23 15:17:34 +0200

    hunt-group-db-connection

--- packages/apps/hunt-group/src/db.js
+++ packages/apps/hunt-group/src/db.js
@@ -1,11 +1,6 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: process.env.SQL_USER,
-    host: process.env.SQL_HOST,
-    database: process.env.SQL_DATABASE,
-    password: process.env.SQL_PASSWORD,
-    port: process.env.SQL_PORT,
     max: process.env.SQL_POOL_MAX_CONN,
     idleTimeoutMillis: process.env.SQL_POOL_MAX_IDLE_CONN,
     connectionTimeoutMillis: process.env.SQL_POOL_CONNECTIONTIMEOUTMILLIS,
