0ee445714430b4865c24c36b773a90be941bd82b
Gabriel Gontariu <gabi@openline.ai>
2022-12-23 14:57:34 +0200

    remove-db-init

--- packages/apps/hunt-group/src/dbInit.js
+++
@@ -1,7 +0,0 @@
-const db = require('./db')
-const fs = require('fs');
-
-exports.dbInit = async () => {
-    const sql = fs.readFileSync('sql/hunt-group.sql').toString();
-    return await db.query(sql)
-};
