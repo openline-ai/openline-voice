ef5f0cc499e87f3740c2b155e5f22d246972fb1a
tsearle <torrey@openline.ai>
2023-01-03 17:25:32 +0000

    switch to inserting into database

--- packages/server/kamailio/scripts/kamailio_network_setup.sh
+++ packages/server/kamailio/scripts/kamailio_network_setup.sh
@@ -4,5 +4,5 @@ sed "s/#MY_PUBLIC_IP_ADDR#/$PUBLIC_IP/g" /etc/kamailio/network.conf.template| se
 
 if [ -n "$ASTERISK_HOST" ]
 then
-    echo "0 sip:$ASTERISK_HOST 0 0" >> /etc/kamailio/dispatcher.list
+echo "INSERT INTO kamailio_dispatcher (id, setid, destination, flags, priority) VALUES (1, 0, 'sip:$ASTERISK_HOST', 8, 0);" |PGPASSWORD=$SQL_PASSWORD  psql -h $SQL_HOST $SQL_USER $SQL_DATABASE
 fi
